d8a435ac7780f3be25709bd4a098d11b7ee2e750
Surabhi <surabhimestry18@gmail.com>
2022-06-24 15:08:28 +0530

    blank entry, strike and focus

--- script.js
+++ script.js
@@ -9,36 +9,45 @@ inptxt.addEventListener("keydown",function(event){
     }
 })
 function addtask(){
-    var divParent = document.createElement("div");
-    var divChild = document.createElement("div");
-    var check = document.createElement("i");
-    var trash= document.createElement("i");
-    
-    divParent.className = "item";
-    divParent.innerHTML = '<div id="strike">'+inptxt.value+'</div>'
-
-    check.className = "fa-solid fa-check";
-    check.style.color = "black";
-    check.addEventListener("click", function(){
-        check.style.color = "limegreen";
-        let text = document.getElementById("strike").innerHTML;
+    if(inptxt.value == ""){
+        alert("Task cannot be blank!");
+        document.getElementById("inptxt").focus();
+    }
+    else{    
+        var divParent = document.createElement("div");
+        var divChild = document.createElement("div");
+        var check = document.createElement("i");
+        var trash= document.createElement("i");
         
-        document.getElementById("strike").innerHTML = "<strike>"+text+"</strike>";
-    })
+        divParent.className = "item";
+        divParent.innerHTML = '<div>'+inptxt.value+'</div>'
 
-    divChild.appendChild(check);
+        check.className = "fa-solid fa-check";
+        check.style.color = "black";
+        check.addEventListener("click", function(){
+            if(check.style.color == "black"){
+                check.style.color = "limegreen";
+                divParent.style.textDecoration = "line-through";
+            }
+            else{
+                check.style.color = "black";
+                divParent.style.textDecoration = "none";
+            }
+        })
 
-    trash.className = "fa-solid fa-trash";
-    trash.style.color = "black";
-    trash.addEventListener("click", function(){
-        divParent.remove();
-    })
+        divChild.appendChild(check);
 
-    divChild.appendChild(trash);
+        trash.className = "fa-solid fa-trash";
+        trash.style.color = "black";
+        trash.addEventListener("click", function(){
+            divParent.remove();
+        })
 
-    divParent.appendChild(divChild);
+        divChild.appendChild(trash);
 
-    tasks.appendChild(divParent);
+        divParent.appendChild(divChild);
 
-    inptxt.value='';
+        tasks.appendChild(divParent);
+        document.getElementById("inptxt").focus();
+        inptxt.value='';}
 }
